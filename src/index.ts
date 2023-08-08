1915e42c5db10f0139f2f534e326049169292a7b
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-08-08 18:32:11 +0100

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,75 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-import confetti from 'canvas-confetti';
+import {v4 as uuidv4} from "uuid";
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+interface TaskCheck {
+  id: string,
+  title: string,
+  completed: boolean,
+  createdAt: Date
+}
+
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.getElementById("new-task-form") as HTMLFormElement | null;
+const inputValue = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: TaskCheck[]= loadTask();
+tasks.forEach(addListItem);
+
+
+
+
+form?.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    if(inputValue?.value == "" || inputValue?.value == null) return
+
+    const newTask: TaskCheck = {
+      id: uuidv4(),
+      title: inputValue.value,
+      completed: false,
+      createdAt: new Date()
+    }
+
+    tasks.push(newTask);
+    saveTask();
+    addListItem(newTask);
+    inputValue.value ="";
+
+})
+
+
+
+function addListItem(task: TaskCheck){
+    const item= document.createElement("li");
+    const label= document.createElement("label");
+    const checkbox= document.createElement("input");
+
+    checkbox.addEventListener('change', ()=>{
+        task.completed = checkbox.checked;
+        saveTask();
+    })
+    checkbox.type= "checkbox";
+    checkbox.checked= task.completed;
+
+    label.append(checkbox, task.title);
+    item.appendChild(label);
+    list?.appendChild(item);
+}
+
+
+
+
+function saveTask(){
+    const stringify = JSON.stringify(tasks);
+     localStorage.setItem('my-tasks', stringify);
+}
+
+function loadTask():TaskCheck[]{
+    const allTaskJSON= localStorage.getItem('my-tasks');
+    if(allTaskJSON == null){
+      return [];
+    }else{
+      const taskOBT= JSON.parse(allTaskJSON);
+      return taskOBT;
+    }
+    
+}
